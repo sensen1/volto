201e75b5a93f3beaf0773fb5210e7b025e983de1
sensen <kwsensen@163.com>
2022-12-04 23:44:39 +0800

    ee

---
+++ src/customizations/components/theme/Footer/Footer.jsx
@@ -0,0 +1,82 @@
+/**
+ * Footer component.
+ * @module components/theme/Footer/Footer
+ */
+
+ import React from 'react';
+ import { Container, List, Segment } from 'semantic-ui-react';
+ import { FormattedMessage, defineMessages, injectIntl } from 'react-intl';
+ import { useSelector, shallowEqual } from 'react-redux';
+ import config from '@plone/volto/registry';
+ 
+ /**
+  * Component to display the footer.
+  * @function Footer
+  * @param {Object} intl Intl object
+  * @returns {string} Markup of the component
+  */
+ const Footer = ({ intl }) => {
+   const { settings } = config;
+   const { lang, siteActions = [] } = useSelector(
+     (state) => ({
+       lang: state.intl.locale,
+       siteActions: state.actions?.actions?.site_actions,
+     }),
+     shallowEqual,
+   );
+ 
+   return (
+     <Segment
+       role="contentinfo"
+       vertical
+       padded
+       inverted
+       color="grey"
+       textAlign="left"
+       id="footer"
+     >
+       <Container>
+         <Segment basic inverted color="grey" className="discreet">
+           <FormattedMessage
+             id="copyright"
+             defaultMessage="Copyright 2022 by {BHU}"
+             values={{
+               BHU: (
+                 <a className="item" href="http://www.bhu.edu.cn">
+                   <FormattedMessage
+                     id="bhu"
+                     defaultMessage="BHU"
+                   />
+                 </a>
+               ),
+             }}
+           />{' '}
+         </Segment>
+         <List horizontal inverted>
+           <div role="listitem" className="item">
+             <a className="item" href="mailto:zhangfang@qymail.bhu.edu.cn">
+               <FormattedMessage
+                 id="contatus"
+                 defaultMessage=" 联系我们"
+               />
+             </a>
+           </div>
+         </List>
+       </Container>
+     </Segment>
+   );
+ };
+ 
+ /**
+  * Property types.
+  * @property {Object} propTypes Property types.
+  * @static
+  */
+ Footer.propTypes = {
+   /**
+    * i18n object
+    */
+ };
+ 
+ export default injectIntl(Footer);
+ 
